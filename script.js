2eca35687a36c62d48448f10e3d77a349beedcbc
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-02-17 17:13:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,32 +2,26 @@
 var btn=document.getElementById("btn");
 var numPtag=document.getElementById("num");
 var res=document.getElementById("respond");
-var inputvalue=document.getElementById("guess").value;
+var inputvalue=doucment.getElemnetById("guess").value;
 
-function randomNumGenerator() {
-    randomNum=Math.floor(Math.random()*41)-20;
+function randomNumGenerator(){
+	randomNum=Math.floor(Math.random()*41)-20;
 	document.getElementById("num").innerText=randomNum;
 	guessNum();
 	checkGame();
-	
+}
+function guessNum(){
+	input=Number(document.getElementById("guess").value);
 }
 
-function guessNum() {
-	input=Number(document.getElementById("guess").value);	
+function checkGame(){
+	if(Math.abs(randomNum-input)<=5)
+	{
+		document.getElementById("respond").innerText="Hot";
+	}
+	else
+	{
+		document.getElementById("respond").innerText="Cold";
+	}
 }
-
-
-function checkGame() {
-
-if(Math.abs(randomNum- input)<=5)
-{
-	documet.getElementById("respond").innerText="Hot";
-}
-else
-{
-	documet.getElementById("respond").innerText="Cold";
-}
-	
-}
-
 btn.addEventListener("click",randomNumGenerator);
